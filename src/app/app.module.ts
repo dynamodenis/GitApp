b98cfde2613e218a9f3d85ea6c57af04ba65e301
Dynamo Denis <dmbugua66@gmail.com>
2020-04-10 17:18:46 +0300

    created the button clicks functions for username and repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }  from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitappComponent } from './gitapp/gitapp.component';
 import { UsernameComponent } from './username/username.component';
 import { RepoComponent } from './repo/repo.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { RepoComponent } from './repo/repo.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
--- src/app/gitapp/gitapp.component.ts
+++ src/app/gitapp/gitapp.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubApiService} from '../github-api.service';
+import{ FormGroup,FormControl} from '@angular/forms'
+
 
 @Component({
   selector: 'app-gitapp',
@@ -7,12 +9,31 @@ import { GithubApiService} from '../github-api.service';
   styleUrls: ['./gitapp.component.css']
 })
 export class GitappComponent implements OnInit {
+//  CREATED A USER NAME OBJECT AND CRETED A A FUNCTION TO RETRIEVE THE INPUT DATA
+  username= new FormGroup({
+    user:new FormControl('')
+  })
+  nameSubmit(){
+    console.log(this.username.value)
+  }
 
+
+//  CREATED A USER NAME OBJECT AND CRETED A A FUNCTION TO RETRIEVE THE INPUT DATA
+  repositories=new FormGroup({
+    repository:new FormControl('')
+  })
+  
+  repoSubmit(){
+    console.log(this.repositories.value)
+  }
+  // CONSTRACTOR TO RETIEVE ALL INJECTIONS
   constructor(private api:GithubApiService) { }
 
   ngOnInit() {
     console.log(this.api.full)
   }
+
+  // STYLE SHEETS BINDING
   myStyles={
     'background-color':'black',
     'color':'white',
@@ -21,9 +42,13 @@ export class GitappComponent implements OnInit {
     'padding-top': '20px'
   }
   body={
-    'height':'auto',
     'font-size':'1.5em',
     'font-family':'italics'
+  }
+  form={
+    'padding-top':'20px',
+    'background-color':'burlywood',
+    'padding':'60px'
   }
   footer={
     'background-color':'black',
