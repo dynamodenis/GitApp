e4c890d87be627d76999528883555df4573de67e
Dynamo Denis <dmbugua66@gmail.com>
2020-04-13 15:49:11 +0300

    added environmental variables in production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  lient_id:"01f5aac10020e9251f16",
+  client_secret:'194626a4360a191f8a4c9aba4f3b47f767e221d4',
+    access_token:'a33bc89a85c8765c06a47f5537bd9e9432179544',
 };
